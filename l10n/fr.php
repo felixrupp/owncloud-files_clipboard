b7162b220cf94b735d54fd3d40be600ecbd845d9
Sugaroverdose <Sugaroverdose@SugarBook.local>
2014-12-22 14:39:33 +0300

    Fix typo

--- l10n/fr.php
+++ l10n/fr.php
@@ -9,7 +9,7 @@
 "Unable to paste: you do not have the permissions to create files in this directory." => "Impossible de coller : vous n'avez pas les permissions pour créer des fichiers dans ce répertoire.",
 "_Paste %n item_::_Paste %n items_" => array("Coller un élément","Coller %n éléments"),
 "An error occurred during the move." => "Une erreur s'est produite lors du déplacement.",
-"Une erreur s'est produite during the copy." => "Une erreur s'est produite lors de la copie.",
+"An error occurred during the copy." => "Une erreur s'est produite lors de la copie.",
 "Processing. Leaving the page now will interrupt the move." => "Traitement en cours. Quitter la page maintenant va interrompre le déplacement.",
 "Processing. Leaving the page now will interrupt the copy." => "Traitement en cours. Quitter la page maintenant va interrompre la copie.",
 "The contents of the clipboard is in conflicts with elements already present in this directory. Do you want to replace them ?" => "Le contenu du presse-papier entre en conflit avec des éléments déjà présent à cet emplacement. Souhaitez-vous les remplacer ?"
