9649801a83fa36da39b94183f33297785e630c93
leizh <leizh@free.fr>
2016-08-27 20:11:12 +0200

    Owncloud 9 compatibility (thanks to bjoernv), use of OC\Files\View for file system operations.

--- ajax/clipboard.php
+++ ajax/clipboard.php
@@ -4,48 +4,22 @@ OCP\JSON::callCheck();
 
 $l = \OC::$server->getL10N('files_clipboard');
 
+$user = OCP\User::getUser();
+$view = new \OC\Files\View();
+
 $required = array('operation', 'directory', 'files', 'destination');
 if(count(array_intersect_key(array_flip($required), $_POST)) !== count($required)) {
 	OCP\JSON::error();
 	exit();
 }
 
-function copyr($src, $dest){
-	if (!\OC\Files\Filesystem::is_dir($src)) {
-		return \OC\Files\Filesystem::copy($src, $dest);
-	} else {
-		if (($dh = \OC\Files\Filesystem::opendir($src)) !== false) {
-			if (!\OC\Files\Filesystem::mkdir($dest)) return false;
-			while (($file = readdir($dh)) !== false) {
-				if ($file == "." || $file == "..") continue;
-				if (!copyr($src.'/'.$file, $dest.'/'.$file)) return false;
-			}
-		}
-		return true;
-	}
-}
-
-function unlinkr($dir){
-	if (!\OC\Files\Filesystem::is_dir($dir)) \OC\Files\Filesystem::unlink($dir);
-	else {
-		$dh = \OC\Files\Filesystem::opendir($dir);
-		while (($file = readdir($dh)) !== false) {
-			if ($file == "." || $file == "..") continue;
-			unlinkr($dir.'/'.$file);
-		}
-		\OC\Files\Filesystem::rmdir($dir);
-	}
-	return !\OC\Files\Filesystem::file_exists($dir);
-}
-
 $messages = array();
 $cut = $_POST['operation'] == 'cut';
-$l = OC_L10N::get('files_clipboard');
 foreach($_POST['files'] as $file) {
-	$source = \OC\Files\Filesystem::normalizePath(stripslashes($_POST['directory']) . '/' . $file);
-	$target = \OC\Files\Filesystem::normalizePath(stripslashes($_POST['destination']) . '/' . $file);
+	$source = $user . "/files/" . \OC\Files\Filesystem::normalizePath(stripslashes($_POST['directory']) . '/' . $file);
+	$target = $user . "/files" . \OC\Files\Filesystem::normalizePath(stripslashes($_POST['destination']) . '/' . $file);
 
-	if (!\OC\Files\Filesystem::file_exists($source)) {
+	if (!$view->file_exists($source)) {
 		$messages[] = $l->t("Unable to paste '%s' item does not exists", array($file));
 		continue;
 	}
@@ -59,25 +33,25 @@ foreach($_POST['files'] as $file) {
 		continue;
 	}
 
-	if (\OC\Files\Filesystem::file_exists($target)) {
-		if (!unlinkr($target)) {
+	if ($view->file_exists($target)) {
+		if (!$view->unlink($target)) {
 			$messages[] = $l->t("Could not remove '%s'", array($file));
 			continue;
 		}
 	}
 
 	if ($cut) {
-		if (!\OC\Files\Filesystem::rename($source, $target)) {
+		if (!$view->rename($source, $target)) {
 			$messages[] = $l->t("Could not move '%s'", array($file));
 		}
 	} else {
-		if (!copyr($source, $target)) {
+		if (!$view->copy($source, $target)) {
 			$messages[] = $l->t("Could not copy '%s'", array($file));
 		}
 	}
 	$error = error_get_last();
 	if ($error) $messages[] = $error->message;
 }
+
 if (empty($messages)) OCP\JSON::success();
 else OCP\JSON::error(array("messages" => $messages));
-
