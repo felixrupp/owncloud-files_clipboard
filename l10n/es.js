0550cae92fb809cbdfda00a5c336773caafcde0b
Leizh <leizh@free.fr>
2018-01-06 09:57:17 +0100

    Spanish translation

---
+++ l10n/es.js
@@ -0,0 +1,19 @@
+OC.L10N.register(
+    "files_clipboard",
+    {
+    "Could not remove '%s'" : "No se ha podido eliminar '%s'",
+    "Could not move '%s'" : "No se ha podido mover '%s'",
+    "Could not copy '%s'" : "No se ha podido copiar '%s'",
+    "Cut" : "Cortar",
+    "Copy" : "Copiar",
+    "Paste" : "Pegar",
+    "Unable to paste: the files come from this directory." : "No se ha podido pegar: los archivos vienen de esta carpeta.",
+    "Unable to paste: you do not have the permissions to create files in this directory." : "No se ha podido copiar: no tienes permisos para crear archivos en esta carpeta.",
+    "_Paste %n item_::_Paste %n items_" : "[ Pegar %n objeto,Pegar %n objetos]",
+    "An error occurred during the move." : "Ha ocurrido un error al mover.",
+    "Une erreur s'est produite during the copy." : "Ha ocurrido un error al copiar.",
+    "Processing. Leaving the page now will interrupt the move." : "Procesando. Abandonar la página ahora interrumpirá la acción de mover.",
+    "Processing. Leaving the page now will interrupt the copy." : "Procesando. Abandonar la página ahora interrumpirá la copia.",
+    "The contents of the clipboard is in conflicts with elements already present in this directory. Do you want to replace them ?" : "Los contenidos del portapapeles está en conflicto con elementos ya presentes en esta carpeta. ¿Quieres reemplazarlos?"
+},
+"");
