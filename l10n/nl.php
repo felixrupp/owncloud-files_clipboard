f6d89fc9ffd3e391548f34eb9c9b12d070bec4c7
Leizh <leizh@free.fr>
2018-01-06 10:32:51 +0100

    Dutch translation

---
+++ l10n/nl.php
@@ -0,0 +1,16 @@
+<?php $TRANSLATIONS = array(
+"Could not remove '%s'" => "'%s' kon niet worden verwijderd.",
+"Could not move '%s'" => "'%s' kon niet worden verplaatst.",
+"Could not copy '%s'" => "'%s' kon niet worden gekopieerd.",
+"Cut" => "Knippen",
+"Copy" => "Kopiëren",
+"Paste" => "Plakken",
+"Unable to paste: the files come from this directory." => "Plakken niet megelijk: de bestanden komen uit deze folder.",
+"Unable to paste: you do not have the permissions to create files in this directory." => "Plakken niet mogelijk: u bent niet gemachtigd om bestanden in deze folder aan te maken.",
+"_Paste %n item_::_Paste %n items_" => array("Een element plakken","%n elementen plakken"),
+"An error occurred during the move." => "Bij het verplaatsen is een fout opgetreden.",
+"An error occurred during the copy." => "Bij het kopiëren is een fout opgetreden.",
+"Processing. Leaving the page now will interrupt the move." => "Verwerking: Als u de pagina nu verlaat, dan wordt het verplaatsen afgebroken.",
+"Processing. Leaving the page now will interrupt the copy." => "Verwerking: Als u de pagina nu verlaat, dan wordt het kopiëren afgebroken.",
+"The contents of the clipboard is in conflicts with elements already present in this directory. Do you want to replace them ?" => "De inhoud van het clipboard is in deze folder reeds aanwezig. Dient deze te worden vervangen?"
+);
