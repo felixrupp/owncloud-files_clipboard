342b67000e3a1d4c3c7351ad0c93504cb2fb1b13
mangelft <miguel.angel.flores@csuc.cat>
2020-10-28 12:00:04 +0100

    Add Catalan translation files

---
+++ l10n/ca.php
@@ -0,0 +1,17 @@
+<?php $TRANSLATIONS = array(
+	"Could not remove '%s'" => "No s'ha pogut eliminar '%s'",
+	"Could not move '%s'" => "No s'ha pogut moure '%s'",
+	"Could not copy '%s'" => "No s'ha pogut copiar '%s'",
+	"Cut" => "Retallar",
+	"Copy" => "Copiar",
+	"Paste" => "Enganxar",
+	"Unable to paste: the files come from this directory." => "No s'ha pogut enganxar: els arxius vénen d'aquesta carpeta.",
+	"Unable to paste: you do not have the permissions to create files in this directory." => "No s'ha pogut copiar: no tens permisos per a crear arxius en aquesta carpeta.",
+	"_Paste %n item_::_Paste %n items_" => array("Enganxar %n objecte","Enganxar %n objectes"),
+	"An error occurred during the move." => "Ha ocorregut un error en moure.",
+	"An error occurred during the copy." => "Ha ocorregut un error en copiar.",
+	"Processing. Leaving the page now will interrupt the move." => "Processant. Abandonar la pàgina ara interromprà l'acció de moure.",
+	"Processing. Leaving the page now will interrupt the copy." => "Processant. Abandonar la pàgina ara interromprà la còpia.",
+	"The contents of the clipboard is in conflicts with elements already present in this directory. Do you want to replace them ?" => "Els continguts del porta-retalls estan en conflicte amb elements ja presents en aquesta carpeta. Vol reemplaçar-los?"
+);
+
