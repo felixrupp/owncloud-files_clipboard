0342e0c5319720d6e56fa0c84b254be6bae7f215
mangelft <miguel.angel.flores@csuc.cat>
2020-10-29 12:03:06 +0100

    Add Catalan translation files

--- l10n/ca.js
+++ l10n/ca.js
@@ -1,20 +1,19 @@
 OC.L10N.register(
-	    "files_clipboard",
-	    {
-		        "Could not remove '%s'" : "No s'ha pogut eliminar '%s'",
-		        "Could not move '%s'" : "No s'ha pogut moure '%s'",
-		        "Could not copy '%s'" : "No s'ha pogut copiar '%s'",
-		        "Cut" : "Retallar",
-		        "Copy" : "Copiar",
-		        "Paste" : "Enganxar",
-		        "Unable to paste: the files come from this directory." : "No s'ha pogut enganxar: els arxius vénen d'aquesta carpeta.",
-		        "Unable to paste: you do not have the permissions to create files in this directory." : "No s'ha pogut copiar: no té permisos per a crear arxius en aquesta carpeta.",
-		        "_Paste %n item_::_Paste %n items_" : "[ Enganxar %n objecte,Enganxar %n objectes]",
-		        "An error occurred during the move." : "Ha ocorregut un error en moure.",
-		        "An error occurred during the copy." : "Ha ocorregut un error en copiar.",
-		        "Processing. Leaving the page now will interrupt the move." : "Processant. Abandonar la pàgina ara interromprà l'acció de moure.",
-		        "Processing. Leaving the page now will interrupt the copy." : "Processant. Abandonar la pàgina ara interromprà la còpia.",
-		        "The contents of the clipboard is in conflicts with elements already present in this directory. Do you want to replace them ?" : "Els continguts del porta-retalls estan en conflicte amb elements ja presents en aquesta carpeta. Vol reemplaçar-los?"
-	    },
-	"");
-
+    "files_clipboard",
+    {
+    "Could not remove '%s'" : "No s'ha pogut eliminar '%s'",
+    "Could not move '%s'" : "No s'ha pogut moure '%s'",
+    "Could not copy '%s'" : "No s'ha pogut copiar '%s'",
+    "Cut" : "Retalla",
+    "Copy" : "Copia",
+    "Paste" : "Enganxa",
+    "Unable to paste: the files come from this directory." : "No es pot enganxar: els fitxers provenen d'aquest directori.",
+    "Unable to paste: you do not have the permissions to create files in this directory." : "No es pot enganxar: no teniu els permisos per crear fitxers en aquest directori",
+    "_Paste %n item_::_Paste %n items_" : "[Enganxa %n element, Enganxa %n elements]",
+    "An error occurred during the move." : "S'ha produït un error durant el moviment.",
+    "An error occurred during the copy." : "S'ha produït un error durant la còpia.",
+    "Processing. Leaving the page now will interrupt the move." : "Processant. Abandonar la pàgina ara interromprà l'acció de moure.",
+    "Processing. Leaving the page now will interrupt the copy." : "Processant. Abandonar la pàgina ara interromprà la còpia.",
+    "The contents of the clipboard is in conflicts with elements already present in this directory. Do you want to replace them ?" : "Els continguts del porta-retalls estan en conflicte amb elements ja presents en aquesta carpeta. Vol reemplaçar-los?"
+},
+"nplurals=2; plural=(n > 1);");
